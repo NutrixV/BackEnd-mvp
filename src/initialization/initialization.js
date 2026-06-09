8873501f024b2932158be26c19222c24cedf952f
Vitali Sorokolit <visor@smile.fr>
2026-06-09 20:37:33 +0300

    Revert "#37: Add Swagger API documentation"

--- src/initialization/initialization.js
+++ src/initialization/initialization.js
@@ -3,12 +3,11 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
 const {
-  config: { CLIENT_URL, SERVER_URL }
+  config: { CLIENT_URL }
 } = require('~/configs/config')
 const router = require('~/routes')
 const { createNotFoundError } = require('~/utils/errorsHelper')
 const errorMiddleware = require('~/middlewares/error')
-const swaggerSetup = require('~/initialization/swaggerSetup')
 
 const initialization = (app) => {
   app.use(express.json({ limit: '10mb' }))
@@ -16,14 +15,13 @@ const initialization = (app) => {
   app.use(cookieParser())
   app.use(
     cors({
-      origin: process.env.NODE_ENV === 'development' ? true : [CLIENT_URL, SERVER_URL].filter(Boolean),
+      origin: process.env.NODE_ENV === 'development' ? true : CLIENT_URL,
       credentials: true,
       methods: 'GET, POST, PATCH, DELETE',
       allowedHeaders: 'Content-Type, Authorization'
     })
   )
 
-  swaggerSetup(app)
   app.use('/', router)
 
   app.use((_req, _res, next) => {
